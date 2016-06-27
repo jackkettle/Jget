3f822e96e6122ade4e4293289e71500e236fa7e0
Jack Kettle <jack.kettle@terminalfour.com>
2016-06-27 16:08:18 +0100

    File system utils update

--- src/main/java/com/jget/core/utils/file/FileSystemUtils.java
+++ src/main/java/com/jget/core/utils/file/FileSystemUtils.java
@@ -1,6 +1,5 @@
 package com.jget.core.utils.file;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -14,32 +13,48 @@ public class FileSystemUtils {
 
     public static boolean pathExists(Path path) {
 
-        File dirFile = path.toFile();
-
-        if (dirFile.exists())
-            return true;
-
-        return false;
+        return Files.exists(path);
 
     }
 
     public static List<Path> populateFilesList(Path path) throws IOException {
-
-        return populateFilesList(path, "");
+        return populateFilesList(path, "", "");
     }
 
-    public static List<Path> populateFilesList(Path path, String filter) throws IOException {
+    public static List<Path> populateFilesListByExtension(Path path, String extension) throws IOException {
+        return populateFilesList(path, extension, "");
+    }
+    
+    public static List<Path> populateFilesListByName(Path path, String name) throws IOException {
+        return populateFilesList(path, "", name);
+    }
+
+    public static List<Path> populateFilesList(Path path, String extension, String fullName) throws IOException {
 
         List<Path> files = new ArrayList<>();
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
             for (Path entry : stream) {
-                if (Files.isDirectory(entry))
-                    files.addAll(populateFilesList(entry, filter));
 
-                if (StringUtils.isEmpty(filter))
+                if (Files.isDirectory(entry))
+                    files.addAll(populateFilesList(path, extension, fullName));
+
+                String fileName = entry.getFileName().toString();
+
+                if (!StringUtils.isEmpty(fullName) && fullName.equals(fileName)) {
                     files.add(entry);
-                else if (entry.toString().endsWith(filter))
+                    continue;
+                }
+
+                if (!StringUtils.isEmpty(fullName) && fileName.toLowerCase().endsWith(extension.toLowerCase())) {
                     files.add(entry);
+                    continue;
+                }
+
+                if (StringUtils.isEmpty(fullName) && StringUtils.isEmpty(extension)) {
+                    files.add(entry);
+                    continue;
+                }
+
             }
         }
         return files;
